5beed997981b843e4b170f13e4bff02b1ae6dc97
Mhoydra <sidemhoidra@gmail.com>
2026-05-21 16:14:30 -0300

    Aula 2 - Criado servidor com Express

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("servidor iniciado");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Pagina inicial com Express');
+});
+
+app.get('/sobre', (req, res) => {
+    res.send('Pagina sobre');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Pagina contato');
+});
+
+app.get('/produtos', (req, res) => {
+    res.send('Lista de produtos');
+});
+
+app.get('/servicos', (req, res) => {
+    res.send('Lista de servicos');
+});
+
+app.listen(3000, () => {
+    console.log("Servidor Rodando e http://localhost:3000")
+});
